62080085a3c5a31cbcf3125a10e494b1a5a35de8
romaSHS <roman.z@outsource-to-ukraine.com>
2021-05-06 18:04:14 +0300

    fixed initial migration: added indexes

--- src/main/kotlin/com/zigzag/jobotaserver/migrations/ChangeLog001_Initial.kt
+++ src/main/kotlin/com/zigzag/jobotaserver/migrations/ChangeLog001_Initial.kt
@@ -4,13 +4,20 @@ package com.zigzag.jobotaserver.migrations
 
 import com.github.cloudyrock.mongock.ChangeLog
 import com.github.cloudyrock.mongock.ChangeSet
+import com.mongodb.BasicDBObject
 import com.mongodb.client.MongoDatabase
+import com.mongodb.client.model.IndexOptions
+
+
+
+
 
 @ChangeLog(order="001")
 class ChangeLog001_Initial {
     @ChangeSet(order = "001", id = "createMyCollection", author = "zigzag")
     fun changeWithMongoDatabase(db: MongoDatabase) {
-         db.createCollection("users")
+         db.createCollection("users");
+         db.getCollection("users").createIndex(BasicDBObject("email", 1), IndexOptions().unique(true));
         //   val doc: Document = Document("testName", "example").append("test", "1")
         //mycollection.insertOne(doc)
     }
